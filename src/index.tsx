21c2609e52ba4dac882dc1d0967868f235488d35
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-05-18 20:23:40 +0900

    github pagesにデプロイするために元に戻す

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
@@ -13,7 +12,14 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const container = document.getElementById("root");
+
+// webpackでバンドルするときに、containerのnullチェックをする必要があったので修正
+// if (container) {
+//   const root = container.hasChildNodes()
+//     ? hydrateRoot(container, <App />)
+//     : createRoot(container);
+//   root.render(<App />);
+// } else {
+//   console.error("Root container not found");
+// }
