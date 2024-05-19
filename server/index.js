00574f677e2aa5e57812c8ede5ea7a6d4cb61786
odendayoko <128268777+odendayoko@users.noreply.github.com>
2024-05-19 12:46:34 +0900

    仮でサーバー設定ファイルを設定

--- server/index.js
+++ server/index.js
@@ -1,11 +1,47 @@
+import express from "express";
+import path from "path";
+import fs from "fs";
+import React from "react";
+import App from "../src/App";
+import ReactDOMServer from "react-dom/server";
+
 // React3
 // Hello worldを表示
-const express = require("express");
+// const express = require("express");
+// const app = express();
+// const port = 9000;
+
+// app.get("/", (req, res) => {
+//   res.send("Hello world");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}/`);
+// });
+
+// React4,5
 const app = express();
 const port = 9000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.use(express.static(path.resolve(__dirname, "../dist")));
+
+app.get("*", (req, res) => {
+  const appHtml = ReactDOMServer.renderToString(<App />);
+
+  const indexFile = path.resolve(__dirname, "../build/index.html");
+
+  fs.readFile(indexFile, "utf8", (err, data) => {
+    if (err) {
+      console.error("Something went wrong:", err);
+      return res.status(500).send("Oops, better luck next time!");
+    }
+
+    const html = data.replace(
+      '<div id="root"></div>',
+      `<div id="root">${appHtml}</div>`
+    );
+    res.status(200).send(html);
+  });
 });
 
 app.listen(port, () => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,93 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.tsx",
-  output: {
-    publicPath: "/",
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+module.exports = [
+  {
+    name: "server",
+    entry: "./server/index.js",
+    mode: "development",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+    },
+    target: "node",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
         },
-      },
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: "ts-loader",
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader, // CSSファイルを個別に出力するためのローダー
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "styles.css",
+      }),
     ],
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
   },
-};
+  {
+    name: "client",
+    mode: "development",
+    entry: "./src/index.tsx",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "client.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        },
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: "ts-loader",
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.scss$/,
+          use: ["css-loader", "sass-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+  },
+];
